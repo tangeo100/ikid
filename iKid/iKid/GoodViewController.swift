0c7b2c37ccb6effc5c23acb740922ed68c91651d
Ronald Tang <rnld.tang@gmail.com>
2018-10-23 14:55:06 -0700

    Initial Commit

---
+++ iKid/iKid/GoodViewController.swift
@@ -0,0 +1,30 @@
+//
+//  GoodViewController.swift
+//  iKid
+//
+//  Created by Ronald Tang on 10/22/18.
+//  Copyright © 2018 Ronald Tang. All rights reserved.
+//
+
+import UIKit
+
+class GoodViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ iKid/iKid/KnockKnockViewController.swift
@@ -0,0 +1,30 @@
+//
+//  KnockKnockViewController.swift
+//  iKid
+//
+//  Created by Ronald Tang on 10/22/18.
+//  Copyright © 2018 Ronald Tang. All rights reserved.
+//
+
+import UIKit
+
+class KnockKnockViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
