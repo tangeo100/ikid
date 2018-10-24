e4770aebf56840028028f8961391df227795b6c6
Ronald Tang <rnld.tang@gmail.com>
2018-10-23 17:42:58 -0700

    Flips Work. AutoLayout Works.

--- iKid/iKid/GoodViewController.swift
+++ iKid/iKid/GoodViewController.swift
@@ -10,21 +10,69 @@ import UIKit
 
 class GoodViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+    fileprivate var joke : GoodJokeView!
+    fileprivate var resolve : GoodResolveViewController!
+    
+    fileprivate func jokeBuilder() {
+        if joke == nil {
+            joke =
+                (storyboard?
+                    .instantiateViewController(withIdentifier: "GoodJoke")
+                    as! GoodJokeView)
+        }
+    }
+    fileprivate func resolveBuilder() {
+        if resolve == nil {
+            resolve =
+                (storyboard?
+                    .instantiateViewController(withIdentifier: "GoodResolve")
+                    as! GoodResolveViewController)
+        }
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func switchViews(_ sender: Any) {
+        resolveBuilder()
+        jokeBuilder()
+        
+        UIView.beginAnimations("View Flip", context: nil)
+        UIView.setAnimationDuration(0.4)
+        UIView.setAnimationCurve(.easeInOut)
+        
+        if joke != nil && joke?.view.superview != nil {
+            UIView.setAnimationTransition(.flipFromRight, for: view, cache: true)
+            resolve.view.frame = view.frame
+            switchViewController(joke, to: resolve)
+        }
+        else {
+            UIView.setAnimationTransition(.flipFromLeft, for: view, cache: true)
+            joke.view.frame = view.frame
+            switchViewController(resolve, to: joke)
+        }
+        
+        UIView.commitAnimations()
     }
-    */
+    
+    fileprivate func switchViewController(_ from: UIViewController?, to: UIViewController?) {
+        if from != nil {
+            from!.willMove(toParent: nil)
+            from!.view.removeFromSuperview()
+            from!.removeFromParent()
+        }
+        
+        if to != nil {
+            self.addChild(to!)
+            self.view.insertSubview(to!.view, at: 0)
+            to!.didMove(toParent: self)
+        }
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        jokeBuilder()
+        switchViewController(nil, to: joke)
+    }
 
 }
---
+++ iKid/iKid/PunnyResolveViewController.swift
@@ -0,0 +1,30 @@
+//
+//  PunnyResolveViewController.swift
+//  iKid
+//
+//  Created by Ronald Tang on 10/23/18.
+//  Copyright © 2018 Ronald Tang. All rights reserved.
+//
+
+import UIKit
+
+class PunnyResolveViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
